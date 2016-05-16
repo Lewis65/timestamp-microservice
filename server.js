1ea045a9ce9e82567d7916dfcb252e37855b9a06
Lewis Horwood <horwood.lewis@gmail.com>
2016-05-15 18:08:29 -0600

    Server functional

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res){
+    res.end("<h1>Timestamp Microservice</h1><p>Please append a query to the url, for example a unix timestamp or natural language timestamp (January%201%2C%202016)</p><p>See the FreeCodeCamp user stories: https://www.freecodecamp.com/challenges/timestamp-microservice</p>")
+})
+
 app.get('/:time', function(req, res){
-    res.end(req.params.time);
+    var query = req.params.time;
+    res.end(query);
 })
 
 app.listen(8080)
